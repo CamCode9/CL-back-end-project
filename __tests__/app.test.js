d5bc320ce022f27e54f1851d24a5fc19b3b1b746
Cam Lindsay <cameronlindsay@Cams-MacBook-Pro.local>
2022-03-28 13:16:07 +0100

    have i broken git?

---
+++ __tests__/app.test.js
@@ -0,0 +1,34 @@
+const request = require("supertest");
+const app = require("../app");
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data");
+
+afterAll(() => db.end());
+beforeEach(() => seed(testData));
+
+describe("GET /api/topics", () => {
+  test("200: Responds with an array of topic objects", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then((result) => {
+        expect(result.body.topics).toBeInstanceOf(Array);
+        result.body.topics.forEach((topic) => {
+          expect(topic).toMatchObject({
+            description: expect.any(String),
+            slug: expect.any(String),
+          });
+        });
+      });
+  });
+  test("404: responds not found for invalid path", () => {
+    return request(app)
+      .get("/api/notTopics")
+      .expect(404)
+      .then((result) => {
+        console.log(result.body);
+        expect(result.body.msg).toBe("Path not found");
+      });
+  });
+});
