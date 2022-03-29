184a29d8777ae11942eaf56d8c765bc7da6fba94
Cam Lindsay <cameronlindsay@Cams-MacBook-Pro.local>
2022-03-29 12:33:53 +0100

    completed patch article votes

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,6 +1,8 @@
+const { checkExists } = require("../db/helpers/utils");
 const {
   selectAllArticles,
   selectArticlesById,
+  updateArticleVoteById,
 } = require("../models/articles.models");
 
 exports.getArticles = async (req, res, next) => {
@@ -8,6 +10,24 @@ exports.getArticles = async (req, res, next) => {
     const { article_id } = req.params;
     const result = await selectArticlesById(article_id, next);
     res.status(200).send({ article: result });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.patchArticle = async (req, res, next) => {
+  try {
+    const voteInc = req.body.inc_votes;
+    const article_id = req.params.article_id;
+    const articleExists = await checkExists(
+      "articles",
+      "article_id",
+      article_id
+    );
+    if (articleExists) {
+      let updatedArticle = await updateArticleVoteById(article_id, voteInc);
+      res.status(200).send({ updatedArticle });
+    }
   } catch (err) {
     next(err);
   }
